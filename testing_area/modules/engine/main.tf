8229592f55d3730ef9ba0cf0ae88411d64caf205
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 <frankrafaelsv1@hotmail.com>
2021-10-13 10:17:31 -0500

    asdasd

--- testing_area/modules/engine/main.tf
+++ testing_area/modules/engine/main.tf
@@ -4,5 +4,5 @@ string1="Linux"
 string2="Hint"
 echo "$string1$string2"
 string3=$string1+$string2
-string3+=" is a good tutorial blog site"
-echo $string3
+string3+=" is a good tutorial blog sites for me"
+echo $string3at 
